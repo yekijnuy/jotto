91fc6923042bf3944f3bd1052c52ba64915bd322
Key Houck <keyyun2028@gmail.com>
2019-09-25 16:39:25 -0400

    Added Bootstrap

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -8,7 +8,7 @@ const GuessedWords = props => {
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
   } else {
-      // we inject the props in our setup within our tests
+    // we inject the props in our setup within our tests
     const guessedWordsRows = props.guessedWords.map((word, index) => (
       <tr key={index} data-test="guessed-word">
         <td>{word.guessedWord}</td>
@@ -18,14 +18,14 @@ const GuessedWords = props => {
     contents = (
       <div data-test="guessed-words">
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className="table table-sm">
+          <thead className="thead-light">
             <tr>
               <th>Guess</th>
               <th>Matching Letters</th>
             </tr>
           </thead>
-          <body>{guessedWordsRows}</body>
+          <tbody>{guessedWordsRows}</tbody>
         </table>
       </div>
     );
