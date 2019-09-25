21987ce3169af9d14ca70f8f5f51e5e0a2911c26
Key Houck <keyyun2028@gmail.com>
2019-09-25 13:54:24 -0400

    Added prop type validation

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -4,12 +4,16 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 // utilize the Enzyme adapter for React
 import EnzymeAdapter from "enzyme-adapter-react-16";
+
 // utility function to be used by all test files
-import { findbyTestAttr, findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 // handing the Congrats component to shallow
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// dangerous to use this since it doesn't match props in your code
+const defaultProps = { success: false };
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component
@@ -18,14 +22,17 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
+  // use and spread key values from defaultProps, override any relevant
+  // with provided props from argument
+  const setupProps = { ...defaultProps, ...props };
   // spread operator will take the key value pairs and make them as
   // attributes {success: true} ===> <Congrats success=true />
-  return shallow(<Congrats {...props} />);
+  return shallow(<Congrats {...setupProps} />);
 };
 
 // always render tests are good first
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -40,4 +47,9 @@ test("renders non-empty congrats message when success prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
