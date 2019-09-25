9e0670904161cf9be7680a906420024c1a9623a7
Key Houck <keyyun2028@gmail.com>
2019-09-25 13:54:40 -0400

    Subtle test changes

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from "check-prop-types";
+
 /**
  * Return node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper
@@ -6,4 +8,16 @@
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  // use npm library, give the props, give the props we are expecting, type of check, name
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+
+  expect(propError).toBeUndefined();
 };
