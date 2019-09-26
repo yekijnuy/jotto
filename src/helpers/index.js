28fc1201d5d10a0affdefe34d15e397abdd9f98a
Key Houck <keyyun2028@gmail.com>
2019-09-26 09:41:54 -0400

    Setup with redux

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,9 +5,10 @@
  * @returns {number} - Number of letters matched between guessed and secret words
  */
 export function getLetterMatchCount(guessedWord, secretWord) {
+    // takes the word, splits into array, then makes key's out of them in an object
   const secretLetterSet = new Set(secretWord.split(""));
   const guessedLetterSet = new Set(guessedWord.split(""));
-  console.log(secretLetterSet);
+
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
     .length;
 }
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function - successReducer
+ * @param {array} state - Array of guess words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state, action) => {
+  return null;
+};
