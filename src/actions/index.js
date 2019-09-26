bb94ae2c7692469d2cdef635ae80a3f0e6ac621e
Key Houck <keyyun2028@gmail.com>
2019-09-26 17:26:31 -0400

    Added Thunk for 'guessedWords' piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,31 @@
+import { getLetterMatchCount } from "../helpers";
+
 export const actionTypes = {
-  CORRECT_GUESS: "CORRECT_GUESS"
+  CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD"
 };
 
 /**
- * @function correctGuess
- * @returns {object} - Action object with type 'CORRECT_GUESS'
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *  and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
  */
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
-}
+export const guessWord = guessedWord => {
+  return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({
+        type: actionTypes.CORRECT_GUESS
+      });
+    }
+  };
+};
--- src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { correctGuess, actionTypes } from "./index";
-
-describe("correctGuess", () => {
-  test("returns an action with type `CORRECT_GUESS`", () => {
-    const action = correctGuess();
-    // can't use toBe for mutable data types.  A deep equal is required, recursively compare
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
